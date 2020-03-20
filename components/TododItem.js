a672620eada394ea9f1e5a8984a80926659aa1b0
Dell\Mabel <robert07ravikumar@gmail.com>
2020-03-21 06:54:09 +0800

    React Native Tutorial #15 - Icons & More Flexbox

--- components/TododItem.js
+++ components/TododItem.js
@@ -1,11 +1,15 @@
 import React , {useState} from 'react';
-import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TododItem({ item , pressHandler}){
 
     return(
-        <TouchableOpacity onPress={() => pressHandler(item.key)}> 
-            <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={18} color='#333'/>
+                <Text styke={styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 
@@ -19,6 +23,10 @@ const styles =StyleSheet.create({
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius : 10
+        borderRadius : 10,
+        flexDirection : 'row'
+    },
+    itemText: {
+        marginLeft: 10
     }
 })
